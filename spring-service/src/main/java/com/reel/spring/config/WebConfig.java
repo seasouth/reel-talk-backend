8326968af6a6e78f0289a2fcde027dc38ca7b348
colesouthern <csouthern333@gmail.com>
2024-10-06 20:16:21 -0400

    Removing slash

--- spring-service/src/main/java/com/reel/spring/config/WebConfig.java
+++ spring-service/src/main/java/com/reel/spring/config/WebConfig.java
@@ -1,18 +1,16 @@
 package com.reel.spring.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SuppressWarnings("unused")
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://reel-talk-kappa.vercel.app/")
+                .allowedOrigins("https://reel-talk-kappa.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
