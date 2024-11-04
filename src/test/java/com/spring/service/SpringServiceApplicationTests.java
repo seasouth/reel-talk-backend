35014ce46e24dd6b16a2185ac32732c4acc5070b
colesouthern <csouthern333@gmail.com>
2024-11-03 22:25:53 -0500

    heroku

--- src/test/java/com/spring/service/SpringServiceApplicationTests.java
+++ src/test/java/com/spring/service/SpringServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.spring.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration 
 class SpringServiceApplicationTests {
 
 	@Test
